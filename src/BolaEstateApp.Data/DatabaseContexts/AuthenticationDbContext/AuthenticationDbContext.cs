9ea90f982f95ad8b831d09e9fae2ee76a40edf70
Afarait Sulaimon Bola <afaraitsulaimon@gmail.com>
2020-05-28 21:05:45 +0100

    added database context to startup.cs

--- src/BolaEstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/BolaEstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,18 +1,14 @@
-using EstateApp.Data.Entities;
+using BolaEstateApp.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace BolaEstateApp.Data.DatabaseContexts.AuthenticationDbContext
-
 {
-     public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
         public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
             : base(options)
         {
-
-
         }
     }
 }
