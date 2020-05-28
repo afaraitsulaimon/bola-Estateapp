932a027e433e0e31cc9d08b1569d87a4b1a04854
Afarait Sulaimon Bola <afaraitsulaimon@gmail.com>
2020-05-28 20:07:29 +0100

    referenced the data and the web app

--- src/BolaEstateApp.Web/Program.cs
+++ src/BolaEstateApp.Web/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace BolaEstateApp
+namespace BolaEstateApp.Web
 {
     public class Program
     {
