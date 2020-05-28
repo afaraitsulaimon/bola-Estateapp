91bb9a7d2d4b7fdb9851456b1a5c8caaf13e6d14
Afarait Sulaimon Bola <afaraitsulaimon@gmail.com>
2020-05-28 21:12:21 +0100

    adjusted the name of the app on line 6 in start.cs

--- src/BolaEstateApp.Web/Startup.cs
+++ src/BolaEstateApp.Web/Startup.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using EstateApp.Data.DatabaseContexts.AuthenticationDbContext;
+using BolaEstateApp.Data.DatabaseContexts.AuthenticationDbContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
